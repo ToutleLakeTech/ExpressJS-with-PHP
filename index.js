ccd0221ac450ead58225b4e79fee280b63591237
The MG | GallearTech <63264652+The-MG@users.noreply.github.com>
2022-02-14 18:50:43 -0800

    yes

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.all(/.+\.php$/, phpExpress.router);
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('PHPExpress app listening at http://%s:%s', host, port);
+  console.log('PHPExpress app listening at http://', host, port);
 });
